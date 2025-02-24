6c9c82f34679a5199f20625afea5d4d4094bca23
Nisargmodi3803 <nisargmodi03@gmail.com>
2025-02-24 18:50:39 +0530

    24/02/2024 Working on My Cart UI design, Integrate increase & decrease product quantity APIs with My Cart page & Accordingly Price generated & Delivery Address part of My Cart page in that Fetch Cities & Delivery time APIs integrated and working on UI.

--- src/main/java/com/example/grocery_shop_backend/Controller/CityController.java
+++ src/main/java/com/example/grocery_shop_backend/Controller/CityController.java
@@ -4,6 +4,7 @@ import com.example.grocery_shop_backend.Dto.CityUpdateDTO;
 import com.example.grocery_shop_backend.Entities.City;
 import com.example.grocery_shop_backend.Service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,16 +19,24 @@ public class CityController
 
     // GET API {Find All Cities}
     @GetMapping("/cities")
-    public List<City> findAllCities()
+    public ResponseEntity<List<City>> findAllCities()
     {
-        return cityService.findAllCities();
+       try{
+           return new ResponseEntity<>(cityService.findAllCities(), HttpStatus.OK);
+       }catch (Exception e){
+           return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+       }
     }
 
     // GET API {Find City By ID}
     @GetMapping("/city/{id}")
-    public City findCityById(@PathVariable int id)
+    public ResponseEntity<City> findCityById(@PathVariable int id)
     {
-        return cityService.findCityById(id);
+        try{
+            return new ResponseEntity<>(cityService.findCityById(id), HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
     }
 
     // PATCH API {Update City}
--- src/main/java/com/example/grocery_shop_backend/Controller/DeliveryTimeSlotController.java
+++ src/main/java/com/example/grocery_shop_backend/Controller/DeliveryTimeSlotController.java
@@ -2,7 +2,10 @@ package com.example.grocery_shop_backend.Controller;
 
 import com.example.grocery_shop_backend.Entities.DeliveryTimeSlot;
 import com.example.grocery_shop_backend.Service.DeliveryTimeSlotService;
+import com.twilio.http.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,15 +22,23 @@ public class DeliveryTimeSlotController
 
     // GET API {Get all Time Slots}
     @GetMapping("/time-slot")
-    public List<DeliveryTimeSlot> getAllTimeSlot()
+    public ResponseEntity<List<DeliveryTimeSlot>> getAllTimeSlot()
     {
-        return deliveryTimeSlotService.getAllTimeSlot();
+        try{
+            return new ResponseEntity<>(deliveryTimeSlotService.getAllTimeSlot(), HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
     }
 
     // GET API {Find Slot by ID}
     @GetMapping("/time-slot/{id}")
-    public DeliveryTimeSlot findSlotById(@PathVariable int id)
+    public ResponseEntity<DeliveryTimeSlot> findSlotById(@PathVariable int id)
     {
-        return deliveryTimeSlotService.findSlotById(id);
+        try{
+            return new ResponseEntity<>(deliveryTimeSlotService.findSlotById(id), HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
     }
 }
